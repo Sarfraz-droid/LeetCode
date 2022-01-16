d9f846eb475291854a39b40f8bbc0b37ad172d7b
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-01-16 19:22:56 +0530

    Time: 195 ms (5.24%), Space: 18 MB (5.24%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int ans = 0;
+        int n = seats.size();
+        
+        vector<int> left(n,-1);
+        vector<int> right(n,-1);
+        int tmp = -1;
+        for(int i = 0;i<n;i++){
+            if(seats[i] == 1){
+                tmp = i;
+            }
+            
+            left[i] = tmp;
+        }
+        
+        tmp = -1;
+        for(int i = n-1;i>=0;i--){
+            if(seats[i] == 1){
+                tmp = i;
+            }
+            
+            right[i] = tmp;
+        }
+        
+        for(int i = 0;i<n;i++){
+            if(seats[i] == 1) continue;
+
+            int dist = INT_MAX;
+            if(right[i] != -1){
+                dist = right[i] - i;
+            }
+            
+            if(left[i] != -1){
+                dist = min(dist,(i - left[i]));
+            }
+            cout<<i<<" "<<left[i]<<" "<<right[i]<<" "<<dist<<endl;
+            ans  = max(ans,dist);
+        }
+        
+        return ans;
+    }
+};
