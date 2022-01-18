ac1d149a17d231e36acf77088cf5ea0cae382404
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-01-18 17:42:10 +0530

    Time: 30 ms (5.23%), Space: 16 MB (30.80%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -8,13 +8,16 @@ public:
         vector<int> right(n);
         
         left[0] = height[0];
-        for(int i = 1;i<n;i++){
-            left[i] = max(height[i],left[i-1]);
-        }
-        
-        right[n-1] = height[n-1];
-        for(int i = n-2;i>=0;i--){    
-            right[i] = max(height[i],right[i+1]);
+        for(int i = 0,j=n-1;i<n;i++,j--){
+            if(i == 0)
+                left[i] = height[i];
+            else
+                left[i] = max(height[i],left[i-1]);
+            
+            if(j == n-1)
+                right[j] = height[j];
+            else
+                right[j] = max(height[j],right[j+1]);   
         }
         
         for(int i = 0;i<n;i++){
