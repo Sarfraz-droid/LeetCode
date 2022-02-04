31b6444b689b8ab9df0800c25b031faa9270f91a
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-04 13:48:03 +0530

    Time: 150 ms (57.35%), Space: 83.8 MB (86.74%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -6,15 +6,15 @@ public:
         int n = nums.size();
         
         unordered_map<int,int> mp;
-        
-        // for(int i = 0;i<(n+1);i++) mp[i] = -1;
-        
+
         for(int i = 0;i<n;i++){
             temp += nums[i] == 0 ? -1 : 1;
-            // cout<<temp<<" - ";
+            
             if(temp == 0)
+            {
                 ans = max(ans,i+1);
-            // cout<<i<<" : "<<temp<<" "<<mp[temp]<<"\n";
+            }
+            
             if(mp[temp] == 0){
                 mp[temp] = i+1;
             }else{
