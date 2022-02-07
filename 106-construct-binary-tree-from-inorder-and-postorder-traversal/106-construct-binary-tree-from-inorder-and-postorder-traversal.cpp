3c73ddb855c9e54b5fb17731102e4bf1b9d34c4e
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-07 19:31:51 +0530

    Time: 83 ms (8.77%), Space: 25.9 MB (87.38%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int postIndex = 0;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        postIndex = postorder.size()-1;
+        
+        return makeTree(inorder,postorder,0,inorder.size()-1);
+        
+    }
+    
+    TreeNode* makeTree(vector<int>& inorder,vector<int>& postorder,int start,int end){
+        if(start > end)
+            return NULL;
+        
+        cout<<postIndex<<"-"<<start<<" "<<end<<" ";    
+        
+        
+        TreeNode* node = new TreeNode(postorder[postIndex]);
+        postIndex--;
+        
+        
+        int mid = midIndex(inorder,start,end,node->val);
+        
+        cout<<mid<<"\n";
+        
+        if(mid!=-1){
+            node->right = makeTree(inorder,postorder,mid+1,end);
+            node->left = makeTree(inorder,postorder,start,mid-1);
+        }
+        
+        return node;
+    }
+    
+    int midIndex(vector<int>& inorder,int start,int end,int val){
+        for(int i = start;i<=end;i++){
+            if(val == inorder[i])
+                return i;
+        }
+        
+        return -1;
+    }
+    
+    
+};
