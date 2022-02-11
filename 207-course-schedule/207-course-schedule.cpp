862fe36dc83e72a8c3c2c89487423d223c7ab1d8
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-12 01:34:18 +0530

    Time: 22 ms (72.18%), Space: 14.4 MB (40.31%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool iscycle(vector<int> adj[], vector<int>& visited, int i)
+    {
+        if(visited[i]==2)//explored and no cycle following this node
+            return false;
+        if(visited[i]==1)//if this node has appeared in the dfs before
+            return true;
+        if(visited[i]==0)
+        {
+            visited[i]=1;
+            for(auto j:adj[i])//all nodes that this nodes leads to
+            {
+                if(iscycle(adj,visited,j))
+                    return true;
+            }
+        }
+        visited[i]=2;//node marked explored and no cycle following this node
+        return false; 
+    }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n];//adjacency matrix for each course
+        //indicates which courses require this prereq
+        for(auto edge: pre)
+        {
+            adj[edge[1]].push_back(edge[0]);
+        }
+        vector<int> visited (n,0);//which course node has been visited
+        for(int i=0;i<n;i++)//checking if i-th course is a part of/joined to a cycle (dfs)
+            if(iscycle(adj,visited,i))
+                return false;
+        return true;
+        
+    }
+
+};
