f309be6584a129c0d2f35a9c627523a6a543a2a9
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-12 03:48:22 +0530

    Time: 50 ms (49.38%), Space: 12.3 MB (84.08%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    void markCurrent(int i,int j,vector<vector<char>>& grid){
+        if(i>= 0 and j>=0 and j<grid[0].size() and i < grid.size() and grid[i][j] == '1'){
+            grid[i][j] = '2';
+            
+            markCurrent(i+1,j,grid);
+            markCurrent(i-1,j,grid);
+            markCurrent(i,j+1,grid);
+            markCurrent(i,j-1,grid);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+    
+        int count = 0;
+        for(int i = 0;i<grid.size();i++){
+            for(int j = 0;j<grid[0].size();j++){
+                if(grid[i][j] == '1'){
+                    cout<<i<<" "<<j<<endl;
+                    markCurrent(i,j,grid);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
