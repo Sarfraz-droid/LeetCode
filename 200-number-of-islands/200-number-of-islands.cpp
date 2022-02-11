a9d48c81a6a4af83334fa0f47f8decdf5c9e0981
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-12 03:49:01 +0530

    Time: 36 ms (77.21%), Space: 12.1 MB (97.09%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -13,7 +13,9 @@ public:
     }
     
     int numIslands(vector<vector<char>>& grid) {
-    
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int count = 0;
         for(int i = 0;i<grid.size();i++){
             for(int j = 0;j<grid[0].size();j++){
