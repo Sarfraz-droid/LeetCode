3fb168ff281002d2bb591be9fce2dd5908f1215d
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-12 01:44:22 +0530

    Time: 28 ms (55.73%), Space: 14.3 MB (40.31%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     bool iscycle(vector<int> adj[], vector<int>& visited, int i)
     {
+        
+        cout<<i<<" ";
+        
         if(visited[i]==2)//explored and no cycle following this node
             return false;
         if(visited[i]==1)//if this node has appeared in the dfs before
             return true;
-        if(visited[i]==0)
-        {
-            visited[i]=1;
-            for(auto j:adj[i])//all nodes that this nodes leads to
+        visited[i]=1;
+        
+        for(auto j:adj[i])//all nodes that this nodes leads to
             {
                 if(iscycle(adj,visited,j))
                     return true;
             }
-        }
+        
         visited[i]=2;//node marked explored and no cycle following this node
         return false; 
     }
@@ -27,8 +29,9 @@ public:
         }
         vector<int> visited (n,0);//which course node has been visited
         for(int i=0;i<n;i++)//checking if i-th course is a part of/joined to a cycle (dfs)
+            {cout<<"\n"<<i<<" ";
             if(iscycle(adj,visited,i))
-                return false;
+                return false;}
         return true;
         
     }
