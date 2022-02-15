330451b98ad15dfc413b7dcec3bd3720a4241c05
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-16 02:40:58 +0530

    Time: 4 ms (100.00%), Space: 17 MB (19.33%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(vector<int>& nums) 
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int ans = 0;
         for(auto x : nums)
             ans ^= x;
