5a75e3017d06a9d7c56004a35572756d735e3f06
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-26 02:18:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.97%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    
+    vector<int> convertString(string str){
+        vector<int> v;
+        stringstream s(str);
+        string word;
+        
+        while(s >> word){
+            v.push_back(stoi(word));
+        }
+        
+        return v;
+    }
+    
+    int compareVersion(string version1, string version2) {
+        replace(version1.begin(),version1.end(),'.',' ');
+        replace(version2.begin(),version2.end(),'.',' ');
+        
+        vector<int> v1 = convertString(version1);
+        vector<int> v2 = convertString(version2);
+        
+//         output(v1);
+//         output(v2);
+        
+        
+        int i = 0,j= 0;
+        int n = v1.size(), m = v2.size();
+        
+        while(i<n and j<m){
+            cout<<v1[i]<<" "<<v2[j]<<endl;
+            
+            if(v1[i] < v2[j]){
+                return -1;
+            }else if(v1[i] == v2[j]){
+                i++;
+                j++;
+            }else{
+                return  1;
+            }
+        }
+        
+        while(i<n){
+            if(v1[i] > 0)
+                return 1;
+            i++;
+        }
+        
+        while(j<m){
+            if(v2[j] > 0)
+                return -1;
+            j++;
+        }
+        
+        return 0;
+    }
+    
+    void output(vector<int> v){
+        for(auto x : v){
+            cout<<x<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+    
+};
