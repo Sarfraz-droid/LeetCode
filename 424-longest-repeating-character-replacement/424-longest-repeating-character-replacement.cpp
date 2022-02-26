148676db658fc7d69357819f39c3e0d610463968
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-27 03:49:08 +0530

    Time: 13 ms (61.52%), Space: 7.1 MB (14.69%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int i = 0, j = 0;
+        int max_cnt = 0,ans = 0;
+        
+        while(j < s.size()){
+            mp[s[j]]++;
+            max_cnt = max(max_cnt,mp[s[j]]);
+            if((j-i+1) - max_cnt > k){
+                mp[s[i]]--;
+                i++;
+            }
+            
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        
+        return ans;
+    
+    }
+};
