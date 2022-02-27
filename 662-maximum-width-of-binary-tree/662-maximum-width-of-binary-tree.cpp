ad10916d20096fec83547b70f3a0af72027dc1a1
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-02-27 13:28:47 +0530

    Time: 97 ms (5.56%), Space: 17.4 MB (21.15%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,38 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-       int maxWidth=0; //maximum width
         
-        if(!root)
-            return maxWidth;
+        queue<pair<TreeNode*,long long>> q;
+        
+        q.push({root,0});
+        int maxAns = 1;
         
         
-        queue<pair<TreeNode* ,int>>que;//this will store a pair of node value with its index
-        que.push({root,0});
         
-        while(!que.empty()){
+        while(!q.empty()){
+            int k = q.size();
+            long long f = -1,l = -1;
+            int size = q.size();    
+            long long levelMin = q.front().second;
+            // cout<<q.size()<<" : ";
             
-            int size=que.size();
-            int levelMinIndex=que.front().second ; 
-            int levelfirstIndex=-1,levellastIndex=-1;
-            
-            for(int i=0;i<size;i++){
-                long long levelCurrIndex=que.front().second -levelMinIndex;
-                TreeNode* node=que.front().first;
-                que.pop();
-                if(i==0)
-                   levelfirstIndex=levelCurrIndex;
-                if(i==size-1)
-                    levellastIndex=levelCurrIndex;
-                if(node->left)
-                    que.push({node->left ,2*levelCurrIndex + 1});
-                if(node->right)
-                    que.push({node->right ,2*levelCurrIndex + 2});
+            while(k--){
+                
+                TreeNode* temp = q.front().first;
+                long long curr = q.front().second - levelMin;
+                q.pop();
+                
+                cout<<k<<":"<<size<<"-"<<curr<<" ";
+                
+                if(k == size-1)
+                    f = curr;
+                else if(k == 0)
+                    l = curr;
+                
+                
+                
+                if(temp->left)
+                    q.push({temp->left,curr*2+1});
+
+                if(temp->right)
+                    q.push({temp->right,curr*2+2});
             }
-            
-            maxWidth = max(maxWidth ,levellastIndex-levelfirstIndex+1 );
+            cout<<"    "<<f<<" "<<l;
+            cout<<endl;
+            if(f != -1 and l != -1)
+            { 
+                long long t = l-f+1;
+                maxAns = maxAns > t ? maxAns : t;
+            }
         }
         
-        return maxWidth;
+        return maxAns;
     }
 };
