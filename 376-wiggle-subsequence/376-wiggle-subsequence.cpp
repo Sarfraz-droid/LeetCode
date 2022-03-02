a412e0fa7513914fc12c9ee41c2af67040511e50
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-03-03 02:45:48 +0530

    Time: 4 ms (52.73%), Space: 7.3 MB (30.00%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,59 @@
+struct Node{
+    bool isIncreasing;
+    int val;
+    
+    Node(bool a,int b){
+        isIncreasing = a;
+        val = b;
+    }
+    
+};
+
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        stack<Node*> st;
+        // st.push(new Node(true,0));
+        int n = nums.size();
+        int i = 0;
+        while(i<n)
+        {
+            if(st.size() == 0){
+                st.push(new Node(false,nums[i]));
+                i++;
+                continue;
+            }else if(st.size() == 1){
+                Node* top = st.top();
+                if(top->val != nums[i]){
+                    bool type = nums[i] > top->val;
+                    st.push(new Node(type,nums[i]));
+                }
+                i++;
+                continue;
+            }
+            
+            Node* top = st.top();
+            
+            if(top->isIncreasing){
+                if(top->val <= nums[i]){
+                    st.pop();
+                    top->val = nums[i];
+                    st.push(top);
+                }else{
+                    st.push(new Node(false,nums[i]));
+                }
+            }else{
+                if(top->val >= nums[i]){
+                    st.pop();
+                    top->val = nums[i];
+                    st.push(top);
+                }else{
+                    st.push(new Node(true,nums[i]));
+                }
+            }
+            i++;
+        }
+        
+        return st.size();
+    }
+};
