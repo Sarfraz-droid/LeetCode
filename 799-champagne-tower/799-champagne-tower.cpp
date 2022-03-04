7519bbefbfce139bf7de204d32a595ba8696b87b
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-03-05 01:27:39 +0530

    Time: 429 ms (5.16%), Space: 43.1 MB (9.79%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+         vector<vector<double>> dp(101,vector<double>(101,0));
+        query_row++;
+        query_glass++;
+        dp[1][1] = poured;
+        for(int i = 2;i<=(query_row);i++){
+            for(int j = 1;j<=i;j++){
+                double sum = 0;
+                if(dp[i-1][j] > 1){
+                    sum+=double(dp[i-1][j]-1)/2;
+                }
+                
+                if(dp[i-1][j-1] > 1){
+                    sum+=double(dp[i-1][j-1]-1)/2;
+                }
+                dp[i][j] = sum;
+            }
+        }
+        
+        for(int i = 1;i<=query_row;i++){
+            
+            for(int j = 1;j<=i;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return dp[query_row][query_glass] > 1 ? double(1) : dp[query_row][query_glass];
+    }
+};
