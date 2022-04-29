bd1e53c509ff039f8a96f740889702425a56b896
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-04-29 20:37:53 +0530

    Time: 36 ms (39.48%), Space: 13.7 MB (41.19%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int n;
+    // vector<bool> visited;
+    vector<int> col;
+    bool helper(vector<vector<int>>& graph,int pos){
+        queue<int> q;
+        q.push(pos);
+
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+            for(auto nbr : graph[f]){
+                if(col[nbr] == -1){
+                    col[nbr] = col[f] == 0 ? 1 : 0;
+                    q.push(nbr);
+                }else if(col[nbr] == col[f]){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    bool isundirected(vector<vector<int>>& graph,int i,int parent){
+        return find(graph[i].begin(),graph[i].end(),parent) == graph[i].end();
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        n = graph.size();
+        
+        // visited.resize(n,false);
+        col.resize(n,-1);
+        
+        for(int i = 0;i<n;i++){
+            if(col[i] == -1 and graph[i].size() > 0){
+                col[i] = 0;
+                if(!helper(graph,i)){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
