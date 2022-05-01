80a3d42b7e75a00e4f5eb5fcc75572cab13f5f5e
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-05-02 03:09:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (77.89%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+         string st=s;    
+        reverse(st.begin(), st.end());
+        
+        if(st==s)
+            return 1;
+
+        return 2;
+        
+    }};
